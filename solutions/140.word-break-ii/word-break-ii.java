9c6ee38f626171bf340a296a1e9e91ff20b9e032
bianximo <937077885@qq.com>
2017-02-20 21:41:04 +0800

    'master'

--- solutions/140.word-break-ii/word-break-ii.java
+++ solutions/140.word-break-ii/word-break-ii.java
@@ -4,6 +4,13 @@ public class Solution {
         map = new HashMap<>();
         return getBreak(s,dict);
     }
+    private boolean containsSuffix(List<String> dict, String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (dict.contains(str.substring(i))) return true;
+        }
+        return false;
+    }
+
     public List<String>getBreak(String s,List<String>dict){
         if(map.containsKey(s))return map.get(s);
         List<String>list = new LinkedList<>();
@@ -12,7 +19,7 @@ public class Solution {
         }
         for(int i=0;i<s.length();i++){
             String left = s.substring(0,i),right = s.substring(i,s.length());
-            if(dict.contains(left)){
+            if(dict.contains(left)&& containsSuffix(dict, right)){
                 List<String>temp = getBreak(right,dict);
                 for(String r : temp){
                     list.add(left+" "+r);
